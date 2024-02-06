23a215a562077a22f38282fa479bc3966b163259
libing <lbingasd@gmail.com>
2024-02-06 09:02:20 +0800

    update html加载方式开关

--- dist-electron/index.js
+++ dist-electron/index.js
@@ -1,1 +1,17 @@
-"use strict";const s=require("electron"),e=require("node:path");process.env.DIST_ELECTRON=__dirname;process.env.DIST=e.join(process.env.DIST_ELECTRON,"../dist");process.env.VITE_PUBLIC=process.env.VITE_DEV_SERVER_URL?e.join(process.env.DIST_ELECTRON,"../public"):process.env.DIST;const o=e.join(process.env.DIST,"index.html");s.app.whenReady().then(()=>{const n=new s.BrowserWindow({title:"Main window"});process.env.VITE_DEV_SERVER_URL?n.loadURL(process.env.VITE_DEV_SERVER_URL):n.loadFile(o)});
+"use strict";
+const electron = require("electron");
+const node_path = require("node:path");
+process.env.DIST_ELECTRON = __dirname;
+process.env.DIST = node_path.join(process.env.DIST_ELECTRON, "../dist");
+process.env.VITE_PUBLIC = process.env.VITE_DEV_SERVER_URL ? node_path.join(process.env.DIST_ELECTRON, "../public") : process.env.DIST;
+const indexHtml = node_path.join(process.env.DIST, "index.html");
+electron.app.whenReady().then(() => {
+  const win = new electron.BrowserWindow({
+    title: "Main window"
+  });
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(indexHtml);
+  }
+});
